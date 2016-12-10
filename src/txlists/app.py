df4a55b11bca619f9414918a3c789d3232099622
Glyph <glyph@twistedmatrix.com>
2016-12-09 23:48:52 -0800

    try to initialize a database

--- src/txlists/app.py
+++ src/txlists/app.py
@@ -1,6 +1,13 @@
 
-from klein import Klein, Plating
 from twisted.web.template import tags, slot
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from klein import Klein, Plating
+from klein.storage.sql import authorizer_for, open_session_store, tables
+from klein._session import requirer
+
+from sqlalchemy import Column, String, Integer
+import attr
 
 page = Plating(
     defaults={
@@ -14,12 +21,73 @@ page = Plating(
     )
 )
 
+
+@attr.s
+class MessageIngestor(object):
+    _dataStore = attr.ib()
+    _messageTable = attr.ib()
+    _replyTable = attr.ib()
+
+    def ingestSomeMessages(self):
+        return u'messages: ingested'
+
+
+@authorizer_for(MessageIngestor,
+                tables(
+                    message=[
+                        Column("list", String(), index=True),
+                        Column("id", String(), index=True),
+                        # vvv previously "date_timestamp" vvv
+                        Column("received", String(), index=True),
+                        Column("counter", Integer(), index=True),
+                        Column("contents", String()),
+                    ],
+                    reply=[
+                        Column("parent", String(), index=True),
+                        Column("child", String(), index=True),
+                    ],
+                ))
+def authorize_ingestor(metadata, datastore, session_store, transaction,
+                       session):
+    return MessageIngestor(datastore, metadata['message'], metadata['reply'])
+
+
 class ListsManagementSite(object):
     app = Klein()
+
+    @classmethod
+    @inlineCallbacks
+    def makeManagementSite(cls, reactor):
+        procurer = yield open_session_store(reactor,
+                                            "sqlite:///sessions.sqlite",
+                                            [authorize_ingestor.authorizer])
+        returnValue(cls(procurer))
+
+    def __init__(self, procurer):
+        self.procurer = procurer
+
+    @requirer
+    def authorized(self):
+        return self.procurer
 
     @page.routed(app.route("/"),
                  tags.h1("Hello, world!"))
     def root(self, request):
         return {
             "title": "Front Page"
+        }
+
+
+    @authorized(
+        page.routed(app.route("/ingest"),
+                    [tags.h1("Loading..."),
+                     tags.div(slot("ingested"))]),
+        ingestor=MessageIngestor,
+    )
+    def ingest(self, request, ingestor):
+        """
+        Ingest messages (temporary route).
+        """
+        return {
+            "ingested": ingestor.ingestSomeMessages()
         }
--- src/txlists/tap.py
+++ src/txlists/tap.py
@@ -10,14 +10,32 @@ def makeService(options):
     from twisted.internet.endpoints import serverFromString
     from twisted.internet import reactor
     from twisted.web.server import Site
+    from twisted.logger import Logger
+
+    log = Logger()
 
     from txlists.app import ListsManagementSite
 
-    resource = ListsManagementSite().app.resource()
+    from twisted.application.service import MultiService, Service
 
-    return StreamServerEndpointService(
-        serverFromString(reactor, "{}:/certificates:tcp:8443".format(
-            "txsni" if os.environ.get("NO_RENEW") else "le"
-        )),
-        Site(resource)
-    )
+    multiService = MultiService()
+
+    class DeferredStartupService(Service, object):
+        def startService(self):
+            super(DeferredStartupService, self).startService()
+            log.info("requesting service startup")
+            (ListsManagementSite.makeManagementSite(reactor)
+             .addCallback(dbReady))
+    DeferredStartupService().setServiceParent(multiService)
+
+    def dbReady(listsManagementSite):
+        log.info("list management site ready {lms}", lms=listsManagementSite)
+        resource = listsManagementSite.app.resource()
+        StreamServerEndpointService(
+            serverFromString(reactor, "{}:/certificates:tcp:8443".format(
+                "txsni" if os.environ.get("NO_RENEW") else "le"
+            )),
+            Site(resource)
+        ).setServiceParent(multiService)
+
+    return multiService
